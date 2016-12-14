c33da33725917f60569af87d287e217cccb06005
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-14 10:17:03 +0000

    add profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
-  def show 
+
+  def show # 追加
    @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user # ここを修正
+    else
+      render 'new'
+    end
   end
   
   def create
@@ -16,26 +26,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'ユーザー情報を編集しました'
-    else
-      # 保存に失敗した場合は編集画面へ戻す
-      render 'edit'
-    end
-
-  private
+private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation, :biography, :country, :state)
+                                 :password_confirmation)
   end
-  
-  end
-
+  # 中略
 end
