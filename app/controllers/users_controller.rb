4b8f080029cf5dae4b33b76e025236fc22521d48
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-17 11:41:46 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :auth_user, only: [:edit,:update]
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -16,9 +17,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def update
+  end
+  
   private
-
+  
+  def auth_user
+    @user = User.find(params[:id]) #idに一致するUser
+    if current_user != @user
+      redirect_to root_path
+    end
+  end
+  
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
-  get    'signup', to: 'users#new'
-  get    'login' , to: 'sessions#new'
-  post   'login' , to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  resources :users
+  resources :static_pages
 end
