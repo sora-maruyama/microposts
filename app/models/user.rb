ad252d50b8699fb0c39983923ba3a870ac91e59a
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-18 11:07:19 +0000

    test2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+    has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
     has_many :follower_relationships, class_name:  "Relationship",
                                     foreign_key: "followed_id",
                                     dependent:   :destroy
   has_many :follower_users, through: :follower_relationships, source: :follower
-  
+  has_many :microposts
   def feed_items
     Micropost.where(user_id: following_user_ids + [self.id])
   end
