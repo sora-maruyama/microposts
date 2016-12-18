4590fdfeb51badb4fa1ab4a8ae305a45128d22ea
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-18 08:13:21 +0000

    follow microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  has_many :microposts
+    has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
+  
 end
--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Microposts
   class Application < Rails::Application
-    module Microposts
-  class Application < Rails::Application
+
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,7 +24,7 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
 
-    config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
 
