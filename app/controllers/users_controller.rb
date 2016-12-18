56f16083ba535a7f8bd34f17f2444f229d116ee1
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-18 06:59:55 +0000

    enabling profile to be editted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
+  before_action :check_user, only: [:edit, :update]
 
-  def show # 追加
-   @user = User.find(params[:id])
-  end
   
   def new
     @user = User.new
   end
-
-  def create
+  
+  def show
+    @user = User.new
+  end
+  
+  def update
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user # ここを修正
-    else
-      render 'new'
+    redirect_to @user
+     else
+      render 'show'
     end
+  end
+
+  
+  def edit
   end
   
   def create
@@ -26,11 +32,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+
+  
+  
 private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :biography, :country, :state)
   end
   # 中略
 end
+
+  def check_user
+    @user = User.find(params[:id])
+    if @user = current_user
+    else
+      redirect_to root_path
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
+  get 'Profile', to: 'users#edit'
   resources :users
 end
